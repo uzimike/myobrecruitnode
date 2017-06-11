81c6f9081c9e2d46450a4fe151c45b1ea2fc4286
uzimike <michaelktlr78@gmail.com>
2017-06-11 16:39:39 +1200

    made a start on the recruitment form

--- routes/recruit.js
+++ routes/recruit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('recruit', { title: 'Recruitment Plans' });
+  res.render('recruit', { title: 'Recruitment Plans', subtitle: 'New Recruitment Plan' });
 });
 
 module.exports = router;
