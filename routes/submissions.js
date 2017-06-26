3f3ec9142c9f15d4453e7b70c61278ef8b04be39
uzimike <michaelktlr78@gmail.com>
2017-06-26 16:57:24 +1200

    init submission page

--- routes/submissions.js
+++ routes/submissions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('preferences', { title: 'Preferences' });
+  res.render('submissions', { title: 'Submissions' });
 });
 
 module.exports = router;
