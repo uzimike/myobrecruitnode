cf432f4081517cf144d4a5914c410a6cb4bcd644
uzimike <michaelktlr78@gmail.com>
2017-06-28 21:42:29 +1200

    init the new app page

--- routes/newapplication.js
+++ routes/newapplication.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('preferences', { title: 'Preferences' });
+  res.render('newapplication', { title: 'Application Forms', subtitle: 'New Application Form' });
 });
 
 module.exports = router;
