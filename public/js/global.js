d8cb1dca5898afba3fbe2c577c0bfba92d459e85
uzimike <michaelktlr78@gmail.com>
2017-07-05 10:03:21 +1200

    stuff

--- public/js/global.js
+++ public/js/global.js
@@ -40,12 +40,52 @@ $( document ).ready(function() {
   }
   else if (atPage('recruit/my-recruitment-plan')) {
     vex.dialog.open({
-      message: 'Recruitment Plan Created',
+    message: 'Recruitment Plan Created!',
+    input: [
+          '<style>',
+              '.vex-custom-field-wrapper {',
+                  'margin: 1em 0;',
+              '}',
+              '.vex-custom-field-wrapper > label {',
+                  'display: inline-block;',
+                  'margin-bottom: .2em;',
+              '}',
+          '</style>',
+          '<div class="vex-custom-field-wrapper">',
+              '<h3>What do I do now?</h3>',
+              '<p>This link needs to be shared on your desired platforms</p>',
+              '<div class="clipboard">',
+              '<input name="clipboard" type="text" readonly="readonly" value="https://localhost:3443/mikes-business/applications/jenfni39rf39fjmc"/>',
+              '<label for="clipboard">Copy to clipboard</label>',
+              '</div>',
+              '<div class="vex-custom-input-wrapper">',
+                  '<input name="date" type="date" value="ass" />',
+              '</div>',
+          '</div>',
+          '<div class="vex-custom-field-wrapper">',
+              '<label for="color">Color</label>',
+              '<div class="vex-custom-input-wrapper">',
+                  '<input name="color" type="color" value="#ff00cc" />',
+              '</div>',
+          '</div>'
+      ].join(''),
     });
   }
 
   function sameHeight(sameheightclass) {
+    var maxheight = 0;
+    // find max height
+    sameheightclass.each(function(){
+      currentHeight = this.height();
+      if(currentHeight > maxheight) {
+        maxheight = currentHeight;
+      }
+    });
 
+    // apply max height
+    sameheightclass.each(function(){
+      this.height(maxheight);
+    });
   }
 
   function atPage(path) {
