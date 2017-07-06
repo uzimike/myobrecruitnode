2c299b118745a84cdade7eb2ea7ac6f76755483a
Dipen <pateldipe1@ecs.vuw.ac.nz>
2017-07-06 18:53:03 +1200

    update

--- public/js/global.js
+++ public/js/global.js
@@ -31,10 +31,12 @@ $( document ).ready(function() {
         },
         onFinishing: function (event, currentIndex) {
           form.validate().settings.ignore = ":disabled";
+
           return form.valid();
         },
         onFinished: function (event, currentIndex) {
           // form.resetForm();
+
           window.location.replace('/recruit/my-recruitment-plan');
         }
     });
@@ -106,5 +108,56 @@ $( document ).ready(function() {
       label.text(input.val());
     })
   }
+
+
+  $('.field.drag').each(function(){
+      $(this).click(function(){
+          var typeSelector=($(this).find('span').html());
+          $(".form-building-container .fields").append('<div class="field"><a><span class="expand">▼</span><span class="name">Enter Text</span><span class="type">'+typeSelector+'</span></a></div>');
+      });
+  });
+    // target elements with the "draggable" class
+    interact('.draggable')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent",
+                endOnly: true,
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+            },
+            // enable autoScroll
+            autoScroll: true,
+
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function (event) {
+                var textEl = event.target.querySelector('p');
+
+                textEl && (textEl.textContent =
+                    'moved a distance of '
+                    + (Math.sqrt(event.dx * event.dx +
+                        event.dy * event.dy)|0) + 'px');
+            }
+        });
+
+    function dragMoveListener (event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
+
 
 });
